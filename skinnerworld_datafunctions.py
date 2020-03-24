3f2bd8a4212162dbe96359b782a7938589d5a88f
maggieelkin <60636748+maggieelkin@users.noreply.github.com>
2020-03-24 15:56:24 -0400

    final update

--- skinnerworld_datafunctions.py
+++ skinnerworld_datafunctions.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import numpy as np
 
+
 def create_dict(folder):
     """
     Reads all the pickled objects in a folder and creates a dictionary to store all objects
@@ -16,34 +17,35 @@ def create_dict(folder):
                 d[filename.replace('.pkl', '')] = pickle.load(a)
     return d
 
-def show_data(dict):
+
+def show_data(d):
     """
     Function to create a dataframe from dictionary of gridworld objects
     useful to store data
-    :param dict: dictionary of pickled agentdata objects
+    :param d: dictionary of pickled agentdata objects
     :return: dataframe of select variables form agentdata objects
     """
     df = pd.DataFrame()
-    for data in dict:
-        if dict[data].lever == 'Ratio' and dict[data].lever_reward_limit == 0:
+    for data in d:
+        if d[data].lever == 'Ratio' and d[data].lever_reward_limit == 0:
             schedule = 'FR-1'
-        elif dict[data].lever == 'Ratio':
-            schedule = 'FR-'+str(dict[data].lever_reward_limit)
+        elif d[data].lever == 'Ratio':
+            schedule = 'FR-'+str(d[data].lever_reward_limit)
         else:
-            schedule = 'VR-'+str(dict[data].lever_reward_limit)
-        df1 = pd.DataFrame({'Algorithm': dict[data].algorithm,
-                            'Lever': dict[data].lever,
-                            'Lever Limit': dict[data].lever_reward_limit,
+            schedule = 'VR-'+str(d[data].lever_reward_limit)
+        df1 = pd.DataFrame({'Algorithm': d[data].algorithm,
+                            'Lever': d[data].lever,
+                            'Lever Limit': d[data].lever_reward_limit,
                             'Schedule': schedule,
-                            'Epsilon': dict[data].e,
-                            'Gamma': dict[data].gamma,
-                            'alpha': dict[data].alpha,
-                            'lambda': dict[data].lam,
-                            'Episodes': dict[data].episodes,
-                            'Reward': dict[data].total_reward,
-                            'Avg step': np.round(np.mean(dict[data].step_togo_list),2)
+                            'Epsilon': d[data].e,
+                            'Gamma': d[data].gamma,
+                            'alpha': d[data].alpha,
+                            'lambda': d[data].lam,
+                            'Episodes': d[data].episodes,
+                            'Reward': d[data].total_reward,
+                            'Avg step': np.round(np.mean(d[data].step_togo_list), 2)
                             },
-                           index=[dict[data].title])
+                           index=[d[data].title])
         df = df.append(df1)
     return df
 
